db588bc80762971ab167270f26d632cfd4ac6313
Terry Bu <burea1124@gmail.com>
2015-02-18 14:25:31 -0500

    Initial Commit

---
+++ Animations Practice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animations Practice
+//
+//  Created by Terry Bu on 2/18/15.
+//  Copyright (c) 2015 Terry Bu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animations Practice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Animations Practice
+//
+//  Created by Terry Bu on 2/18/15.
+//  Copyright (c) 2015 Terry Bu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
