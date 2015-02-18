40f8c8552bb391ecc193d78885a4042426768bdc
Terry Bu <burea1124@gmail.com>
2015-02-18 15:25:31 -0500

    first commit - trying Xcode for pushing

--- Animations Practice/ViewController.m
+++ Animations Practice/ViewController.m
@@ -17,6 +17,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect rect = CGRectMake(30, 30, 100, 100);
+    UIView *terrySquare = [[UIView alloc]initWithFrame:rect];
+    terrySquare.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:terrySquare];
+    
+    //You can do cool things with options under UIView animateWithDurations
+    
+    //UIViewAnimationOptionRepeat - repeat indefinitely
+    //UIViewAnimationOptionAutoReverse - reverses once when complete
+    
+    //Curve Ease In is exponentially increase in speed
+    //Curve Ease Out is expontentially decrease in speed
+    //Curve Ease In/Out is speed up in the beginning then decrease towards the end
+    
+    
+    [UIView animateWithDuration:3.0 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        terrySquare.frame = CGRectMake(self.view.frame.size.width-100,self.view.frame.size.height-100,100,100);
+    }
+        completion:^(BOOL finished) {
+            NSLog(@"we donezo");
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning {
